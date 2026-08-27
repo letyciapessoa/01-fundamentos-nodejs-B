68f8367623c45c31f74929054226c01cbbaf8f7b
unknown <letycia.pereira2@aluno.cps.sp.gov.br>
2026-08-27 09:10:31 -0300

    Aula 5 persistência de dados

--- server.js
+++ server.js
@@ -7,12 +7,46 @@ return 'Home - Cadastro de usuarios';
 });
 
 server.get('/users', (req, res)=>{
-return 'Listagem de usuarios';
+    const nome = req.query.nome;
+    const email = req.query.email;
+    const telefone = req.query.telefone
+
+    return `Lista de usuarios Filtro: nome: ${nome} - email: ${email} - telefone: ${telefone}`;
+});
+
+
+server.get('/users/id:', (req, res)=>{
+    const id = req.params.id
+return `Detalhes de usuario de id: ${id}`;
 });
 
 server.post('/users', (req, res)=>{
-return 'Cadastro de usuarios';
+    const {nome, email, telefone} = req.body
+
+return {
+    'message': "Cadastro de usuario",
+    nome,
+    email,
+    telefone
+};
 });
+
+server.put('/users/:id',(req, res)=>{
+    const id = req.params.id
+    return `Usuario de id: ${id} atualizado com sucesso`;
+})
+server.delete('/users/:id',(req, res)=>{
+    const id = req.params.id
+    return `Usuario de id: ${id} excluido com sucesso`;
+})
+
+server.get('/users/:userId/pedidos/:pedidoId',(req, res)=>{
+    // const userId = req.params.userId;
+    // const pedidoId = req.params.pedidoId
+    const {userId, periodoId} = req.params
+
+    return `Pedido numero: ${pedidoId} do usuario id: ${userId} realizado com sucesso`;
+})
 
 server.listen({port: 3000}, (err, address) => {
 if (err) {
